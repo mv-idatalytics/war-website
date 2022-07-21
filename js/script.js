81156121600601035220f9c9762bbb673432fdb4
preethi <preethianki@gmail.com>
2022-07-21 13:13:49 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const forms = document.querySelector(".forms"),
+      psdShowHide = document.querySelectorAll(".hide-btn"),
+      links = document.querySelectorAll(".link");
+      
+psdShowHide.forEach(hideBtn => {
+    hideBtn.addEventListener("click", () => {
+        let psdFields = hideBtn.parentElement.parentElement.querySelectorAll(".password");
+
+        psdFields.forEach(password =>{
+            if(password.type === "password"){
+                password.type = "text";
+                hideBtn.classList.replace("bx-hide","bx-show");
+                return;
+            }
+            password.type = "password";
+            hideBtn.classList.replace("bx-show", "bx-hide");
+
+
+        })
+
+
+    })
+}) 
+
+links.forEach(link => {
+    link.addEventListener("click", e=> {
+        e.preventDefault();
+        forms.classList.toggle("show-signup");
+    })
+})
